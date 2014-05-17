95a400cbcfc428f885dfa35ac407ca816ee8609c
Lars Veldscholte <lars@tuxplace.nl>
2014-05-17 22:45:40 +0200

    Steam icons/logos

--- include/classes.php
+++ include/classes.php
@@ -3,15 +3,15 @@ require_once "config.php";
 class game {
 	private $id, $name, $status, $completed, $notes, $color, $purchase;
 
-	function __construct($id, $name, $status, $completed, $notes, $color, $appid, $playtime, $purchase=NULL) {
-		$this->setData($id, $name, $status, $completed, $notes, $color, $appid, $playtime, $purchase);
+	function __construct($id, $name, $status, $completed, $notes, $color, $appid, $playtime, $img_icon_url, $img_logo_url, $purchase=NULL) {
+		$this->setData($id, $name, $status, $completed, $notes, $color, $appid, $playtime, $img_icon_url, $img_logo_url, $purchase);
 	}
 
 	private function getColor() {
 		return $this->color;
 	}
 
-	public function setData($id, $name, $status, $completed, $notes, $color, $appid, $playtime, $purchase) {
+	public function setData($id, $name, $status, $completed, $notes, $color, $appid, $playtime, $img_icon_url, $img_logo_url, $purchase) {
 		$this->id = $id;
 		$this->name = $name;
 		$this->status = $status;
@@ -20,15 +20,25 @@ class game {
 		$this->color = $color;
 		$this->appid = $appid;
 		$this->playtime = round($playtime / 60, 2);
+		$this->img_icon_url = $img_icon_url;
+		$this->img_logo_url = $img_logo_url;
 		$this->purchase = $purchase;
 	}
 
 	public function drawRow($beginrow = 1) {
 		global $currenturl;
 		
+		if(isset($this->purchase)) {
+			$this->img_url = $this->img_icon_url;
+		} else {
+			$this->img_url = $this->img_logo_url;
+		}
+		
 		$string = "";
 		if($beginrow) $string .= ("<tr>");
-		$string .= "<td>{$this->name}</td>";
+		$string .= "<td>";
+		if($this->appid != 0) $string .= "<img src=\"http://media.steampowered.com/steamcommunity/public/images/apps/{$this->appid}/{$this->img_url}.jpg\" /> ";
+		$string .= "{$this->name}</td>";
 		$string .= "<td style=\"background-color: #{$this->getColor()}\">{$this->status}</td>";
 		if(!isset($this->purchase)) $string .= "<td>{$this->appid}</td>";
 		if(!isset($this->purchase)) $string .= "<td>{$this->playtime}</td>";
@@ -105,12 +115,12 @@ class listGames {
 		global $mysqli;
 		$string = "";
 		
-		$this->query = "SELECT purchase_id, game.game_id, game.name, status.name AS status, status.completed, game.notes, status.color, game.appid, game.playtime FROM purchase JOIN xref_purchase_game USING(purchase_id) RIGHT JOIN game USING(game_id) JOIN status USING(status_id) {$this->condition} GROUP BY game.name ORDER BY game.game_id";
+		$this->query = "SELECT purchase_id, game.game_id, game.name, status.name AS status, status.completed, game.notes, status.color, game.appid, game.playtime, game.img_logo_url FROM purchase JOIN xref_purchase_game USING(purchase_id) RIGHT JOIN game USING(game_id) JOIN status USING(status_id) {$this->condition} GROUP BY game.name ORDER BY game.game_id";
 		$this->query2 = "SELECT dlc.dlc_id, dlc.name, status.name AS status, status.completed, dlc.note, status.color FROM dlc JOIN status USING (status_id)";
 		$result = $mysqli->query($this->query);
 
 		while($entries = $result->fetch_assoc()) {
-			$game = new game($entries['game_id'], $entries['name'], $entries['status'], $entries['completed'], $entries['notes'], $entries['color'], $entries['appid'], $entries['playtime']);
+			$game = new game($entries['game_id'], $entries['name'], $entries['status'], $entries['completed'], $entries['notes'], $entries['color'], $entries['appid'], $entries['playtime'], NULL, $entries['img_logo_url']);
 			$string .= $game->drawRow();
 
 			$game_id = $entries['game_id'];
@@ -154,7 +164,7 @@ class listPurchases {
 
 	function __construct() {
 		$this->query = "SELECT * FROM purchase";
-		$this->query2 = "SELECT game.game_id, game.name, game.notes, status.name AS status, status.completed, status.color FROM purchase JOIN xref_purchase_game USING (purchase_id) JOIN game USING (game_id) JOIN status USING (status_id)";
+		$this->query2 = "SELECT game.game_id, game.name, game.notes, game.appid, game.img_icon_url, status.name AS status, status.completed, status.color FROM purchase JOIN xref_purchase_game USING (purchase_id) JOIN game USING (game_id) JOIN status USING (status_id)";
 		$this->query3 = "SELECT dlc.dlc_id, dlc.name, status.name AS status, status.completed, dlc.note, status.color FROM game JOIN dlc USING (game_id) JOIN status on dlc.status_id=status.status_id";
 	}
 	
@@ -178,7 +188,7 @@ class listPurchases {
 			$result2 = $mysqli->query($this->query2 . " WHERE purchase.purchase_id={$entries['purchase_id']}");
 			$j = 0; // draw first game without <tr>
 			while($entries2 = $result2->fetch_assoc()) {
-				$game = new game($entries2['game_id'], $entries2['name'], $entries2['status'], $entries2['completed'], $entries2['notes'], $entries2['color'], NULL, NULL, $entries['purchase_id']);
+				$game = new game($entries2['game_id'], $entries2['name'], $entries2['status'], $entries2['completed'], $entries2['notes'], $entries2['color'], $entries2['appid'], NULL, $entries2['img_icon_url'], NULL, $entries['purchase_id']);
 				$string .= $game->drawRow($j);
 
 				$result3 = $mysqli->query($this->query3 . " WHERE game_id={$entries2['game_id']}");
@@ -236,7 +246,7 @@ function getStatusOptions() {
 function SteamApiRequest() {
 	global $config;
 	
-	$json = file_get_contents("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={$config['steamapikey']}&steamid={$config['steamid']}&format=json&include_appinfo=1");
+	$json = file_get_contents("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={$config['steamapikey']}&steamid={$config['steamid']}&format=json&include_appinfo=1&include_played_free_games=1");
 	return json_decode($json);
 }
 
@@ -258,8 +268,10 @@ function syncSteamPlaytime() {
 	foreach ($steamdata->response->games as $game) {
 		$appid = $game->appid;
 		$playtime = $game->playtime_forever;
+		$icon = $game->img_icon_url;
+		$logo = $game->img_logo_url;
 		
-		$query = "UPDATE game SET playtime=$playtime WHERE appid=$appid";
+		$query = "UPDATE game SET playtime=$playtime, img_icon_url='$icon', img_logo_url='$logo' WHERE appid=$appid";
 		$mysqli->query($query) or die($query); 
 	}
 }
