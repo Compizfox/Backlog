b0057f7bef6132d3024c057a28491406db185a86
Lars Veldscholte <lars@tuxplace.nl>
2014-05-23 01:11:06 +0200

    PHP shorthand echo syntax

--- backlog/index.php
+++ backlog/index.php
@@ -54,10 +54,8 @@ include("include/menu.php");
 				</ul>
 			</nav>
 			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-				<?php
-				echo("<h1>$pagename</h1>");
-				include("pages/$include");
-				?>
+				<h1><?=$pagename?></h1>
+				<?php include("pages/$include") ?>
 			</div>
 		</div>
 	</div>
