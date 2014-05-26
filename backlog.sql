550e08c419e6af6b6c24a900a918ff030034c37a
Lars Veldscholte <lars@tuxplace.nl>
2014-05-26 21:37:05 +0200

    Support for multiple libraries per page (many-to-many relationship)
    Switch to prepared statements

--- backlog.sql
+++ backlog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Gegenereerd op: 26 mei 2014 om 17:20
+-- Gegenereerd op: 26 mei 2014 om 21:34
 -- Serverversie: 5.5.37-1
 -- PHP-versie: 5.5.12-2
 
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `library` (
 `library_id` int(11) NOT NULL,
   `css_url` varchar(255) NOT NULL,
   `js_url` varchar(255) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `library`
@@ -84,7 +84,8 @@ CREATE TABLE IF NOT EXISTS `library` (
 
 INSERT INTO `library` (`library_id`, `css_url`, `js_url`) VALUES
 (1, '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/css/jquery-ui.min.css', '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js'),
-(2, '', '//cdnjs.cloudflare.com/ajax/libs/Chart.js/0.2.0/Chart.min.js');
+(2, '', '//cdnjs.cloudflare.com/ajax/libs/Chart.js/0.2.0/Chart.min.js'),
+(3, '', 'js/autocorrect.php');
 
 -- --------------------------------------------------------
 
@@ -99,35 +100,34 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `scope` varchar(25) NOT NULL,
   `options` varchar(25) NOT NULL,
   `title` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `glyphicon` varchar(255) NOT NULL,
-  `library_id` int(11) DEFAULT NULL
+  `glyphicon` varchar(255) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=77 ;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `menu`
 --
 
-INSERT INTO `menu` (`id`, `parent_id`, `page`, `scope`, `options`, `title`, `glyphicon`, `library_id`) VALUES
-(50, 1, 'overzicht', '', '', 'Summary', 'glyphicon-stats', 2),
-(54, 1, 'purchases', '', '', 'Purchases', 'glyphicon-shopping-cart', NULL),
-(57, 61, 'games', 'uncompleted', '', 'Uncompleted games', 'glyphicon-exclamation-sign', NULL),
-(58, 61, 'games', 'completed', '', 'Completed games', 'glyphicon-ok-sign', NULL),
-(59, 0, 'add', '', '', 'Add', 'glyphicon-plus', 1),
-(61, 1, 'games', 'all', '', 'Games', 'glyphicon-play', NULL),
-(62, 1, 'dlc', 'all', '', 'DLC', 'glyphicon-download', NULL),
-(63, -1, 'modifygame', '', '', 'Modify game', '', NULL),
-(64, -1, 'games', 'purchase', '', 'Games in purchase', '', NULL),
-(66, -1, 'modifypurchase', '', '', 'Modify purchase', '', NULL),
-(67, -1, 'modifydlc', '', '', 'Modify DLC', '', NULL),
-(68, 62, 'dlc', 'uncompleted', '', 'Uncompleted DLC', 'glyphicon-exclamation-sign', NULL),
-(69, 62, 'dlc', 'completed', '', 'Completed DLC', 'glyphicon-ok-sign', NULL),
-(70, -1, 'dlc', 'game', '', 'DLC in game', '', NULL),
-(71, 0, 'steam', '', '', 'Steam', 'fa fa-steam', NULL),
-(72, 71, 'steam', '', '&syncappids', 'Link games with Steam', 'glyphicon-link', NULL),
-(73, 71, 'steam', '', '&syncplaytime', 'Sync playtime with Steam', 'glyphicon-time', NULL),
-(74, 71, 'steam', '', '&syncicons', 'Retrieve icons/logos', 'glyphicon-picture', NULL),
-(75, 71, 'steam', '', '&addgames', 'Import games from Steam', 'glyphicon-import', NULL),
-(76, 0, 'settings', '', '', 'Settings', 'glyphicon-wrench', NULL);
+INSERT INTO `menu` (`id`, `parent_id`, `page`, `scope`, `options`, `title`, `glyphicon`) VALUES
+(50, 1, 'overzicht', '', '', 'Summary', 'glyphicon-stats'),
+(54, 1, 'purchases', '', '', 'Purchases', 'glyphicon-shopping-cart'),
+(57, 61, 'games', 'uncompleted', '', 'Uncompleted games', 'glyphicon-exclamation-sign'),
+(58, 61, 'games', 'completed', '', 'Completed games', 'glyphicon-ok-sign'),
+(59, 0, 'add', '', '', 'Add', 'glyphicon-plus'),
+(61, 1, 'games', 'all', '', 'Games', 'glyphicon-play'),
+(62, 1, 'dlc', 'all', '', 'DLC', 'glyphicon-download'),
+(63, -1, 'modifygame', '', '', 'Modify game', ''),
+(64, -1, 'games', 'purchase', '', 'Games in purchase', ''),
+(66, -1, 'modifypurchase', '', '', 'Modify purchase', ''),
+(67, -1, 'modifydlc', '', '', 'Modify DLC', ''),
+(68, 62, 'dlc', 'uncompleted', '', 'Uncompleted DLC', 'glyphicon-exclamation-sign'),
+(69, 62, 'dlc', 'completed', '', 'Completed DLC', 'glyphicon-ok-sign'),
+(70, -1, 'dlc', 'game', '', 'DLC in game', ''),
+(71, 0, 'steam', '', '', 'Steam', 'fa fa-steam'),
+(72, 71, 'steam', '', '&syncappids', 'Link games with Steam', 'glyphicon-link'),
+(73, 71, 'steam', '', '&syncplaytime', 'Sync playtime with Steam', 'glyphicon-time'),
+(74, 71, 'steam', '', '&syncicons', 'Retrieve icons/logos', 'glyphicon-picture'),
+(75, 71, 'steam', '', '&addgames', 'Import games from Steam', 'glyphicon-import'),
+(76, 0, 'settings', '', '', 'Settings', 'glyphicon-wrench');
 
 -- --------------------------------------------------------
 
@@ -173,6 +173,26 @@ INSERT INTO `status` (`status_id`, `name`, `completed`, `color`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `xref_menu_library`
+--
+
+CREATE TABLE IF NOT EXISTS `xref_menu_library` (
+  `menu_id` int(11) NOT NULL,
+  `library_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `xref_menu_library`
+--
+
+INSERT INTO `xref_menu_library` (`menu_id`, `library_id`) VALUES
+(50, 2),
+(59, 1),
+(59, 3);
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `xref_purchase_game`
 --
 
@@ -180,7 +200,6 @@ CREATE TABLE IF NOT EXISTS `xref_purchase_game` (
   `purchase_id` int(11) NOT NULL,
   `game_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -214,7 +233,7 @@ ALTER TABLE `library`
 -- Indexen voor tabel `menu`
 --
 ALTER TABLE `menu`
- ADD PRIMARY KEY (`id`), ADD KEY `parent_id` (`parent_id`), ADD KEY `libraries` (`library_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `parent_id` (`parent_id`);
 
 --
 -- Indexen voor tabel `purchase`
@@ -227,6 +246,12 @@ ALTER TABLE `purchase`
 --
 ALTER TABLE `status`
  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexen voor tabel `xref_menu_library`
+--
+ALTER TABLE `xref_menu_library`
+ ADD KEY `library_id` (`library_id`), ADD KEY `menu_id` (`menu_id`);
 
 --
 -- Indexen voor tabel `xref_purchase_game`
@@ -257,7 +282,7 @@ MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT voor een tabel `library`
 --
 ALTER TABLE `library`
-MODIFY `library_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `library_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT voor een tabel `menu`
 --
@@ -300,10 +325,11 @@ ADD CONSTRAINT `history_ibfk_3` FOREIGN KEY (`old_status`) REFERENCES `status` (
 ADD CONSTRAINT `history_ibfk_4` FOREIGN KEY (`new_status`) REFERENCES `status` (`status_id`);
 
 --
--- Beperkingen voor tabel `menu`
+-- Beperkingen voor tabel `xref_menu_library`
 --
-ALTER TABLE `menu`
-ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`library_id`) REFERENCES `library` (`library_id`);
+ALTER TABLE `xref_menu_library`
+ADD CONSTRAINT `xref_menu_library_ibfk_2` FOREIGN KEY (`library_id`) REFERENCES `library` (`library_id`),
+ADD CONSTRAINT `xref_menu_library_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`);
 
 --
 -- Beperkingen voor tabel `xref_purchase_game`
