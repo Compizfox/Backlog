ed03fe25a49ceac093282245023e337a996e0004
Lars Veldscholte <lars@tuxplace.nl>
2015-03-07 21:09:12 +0100

    Added more comments

--- backlog/config.php
+++ backlog/config.php
@@ -5,6 +5,8 @@ $config['user'] = "";
 $config['pass'] = "";
 $config['db']   = "";
 
+# Enter your Steam API key. You can get one here: http://steamcommunity.com/dev/apikey
 $config['steamapikey'] = "";
+# Enter your Steam ID in STEAMID64 format. You can get your Steam ID here: http://steamid.co/
 $config['steamid'] = "";
 ?>
