6f33fff8d7aad1657cc96527bf7b95c41885ddb5
Lars Veldscholte <lars@tuxplace.nl>
2016-09-10 01:32:43 +0200

    Fix selectall checkbox

--- resources/views/purchase/index.blade.php
+++ resources/views/purchase/index.blade.php
@@ -74,3 +74,15 @@
 	</form>
 	@include('includes.delete_purchase')
 @endsection
+
+@push('scripts')
+	<script>
+		$(document).ready(function() {
+			$('#selectall').click(function () {
+				$('.table tbody tr')
+					.find(':checkbox')
+					.prop('checked', this.checked);
+			});
+		});
+	</script>
+@endpush
