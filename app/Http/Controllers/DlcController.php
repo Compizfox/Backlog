8690c17be9afbaafe0a21b0ec4ce5c2e7164a599
Lars Veldscholte <lars@tuxplace.nl>
2016-09-15 19:22:59 +0200

    Remove unnecessary use statements

--- app/Http/Controllers/DlcController.php
+++ app/Http/Controllers/DlcController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\Dlc;
 
 class DlcController extends Controller {
--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\Purchase;
 use App\Game;
 use App\Dlc;
