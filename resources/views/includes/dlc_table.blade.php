da93d2b6fbd52f34e99c76c41aded45722b6d121
Lars Veldscholte <lars@tuxplace.nl>
2016-11-09 21:45:38 +0100

    Visual changes to column order

--- resources/views/includes/dlc_table.blade.php
+++ resources/views/includes/dlc_table.blade.php
@@ -26,25 +26,25 @@
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr>
+			<th><input type="checkbox" class="selectall"></th>
 			<th>Name</th>
 			<th>Game</th>
 			<th>Status</th>
 			<th>Notes</th>
-			<th><input type="checkbox" class="selectall" /></th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($dlcs as $dlc)
 			<tr>
-				<td><a href="{{action('DlcController@show', ['id' => $dlc->id])}}">{{$dlc->name}}</a></td>
-				<td><a href="{{action('GameController@show', ['id' => $dlc->game->id])}}"><img src="{{$dlc->game->getImageUrl('logo')}}" width="184px" height="69px"><img src="{{asset('images/dlc-logo.png')}}"> {{$dlc->game->name}}</a></td>
-				<td style="background-color: {{$dlc->status->color}}">{{$dlc->status->name}}</td>
-				<td>{{$dlc->notes}}</td>
 				<td>
 					<input type="checkbox" name="checkedDlc[]" value="{{$dlc->id}}">&nbsp;
 					<a href="{{action('DlcController@edit', ['id' => $dlc->id])}}"><span class="glyphicon glyphicon-pencil"></span></a>&nbsp;
 					<span class="glyphicon glyphicon-trash clickable delete" data-url="{{action('DlcController@destroy', ['id' => $dlc->id])}}"></span>
 				</td>
+				<td><a href="{{action('DlcController@show', ['id' => $dlc->id])}}">{{$dlc->name}}</a></td>
+				<td><a href="{{action('GameController@show', ['id' => $dlc->game->id])}}"><img src="{{$dlc->game->getImageUrl('logo')}}" width="184px" height="69px"><img src="{{asset('images/dlc-logo.png')}}"> {{$dlc->game->name}}</a></td>
+				<td style="background-color: {{$dlc->status->color}}">{{$dlc->status->name}}</td>
+				<td>{{$dlc->notes}}</td>
 			</tr>
 		@endforeach
 	</tbody>
--- resources/views/includes/game_table.blade.php
+++ resources/views/includes/game_table.blade.php
@@ -26,26 +26,25 @@
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr>
+			<th><input type="checkbox" class="selectall"></th>
 			<th>Name</th>
 			<th>Status</th>
 			<th>Playtime (hours)</th>
 			<th>Notes</th>
-			<th><input type="checkbox" class="selectall"></th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($games as $game)
 			<tr>
-				<td><a href="{{action('GameController@show', ['id' => $game->id])}}"><img src="{{$game->getImageUrl('logo')}}" width="184px" height="69px"> {{$game->name}}</a></td>
-				<td style="background-color: {{$game->status->color}}">{{$game->status->name}}</td>
-				<td>{{$game->getFormattedPlaytime()}}</td>
-				<td>{{$game->notes}}</td>
 				<td>
 					<input type="checkbox" name="checkedGames[]" value="{{$game->id}}">&nbsp;
 					<a href="{{action('GameController@edit', ['id' => $game->id])}}"><span class="glyphicon glyphicon-pencil"></span></a>&nbsp;
 					<span class="glyphicon glyphicon-trash clickable delete" data-url="{{action('GameController@destroy', ['id' => $game->id])}}"></span>
-					<a href="/dlc"><span class="glyphicon glyphicon-download"></span></a>
 				</td>
+				<td><a href="{{action('GameController@show', ['id' => $game->id])}}"><img src="{{$game->getImageUrl('logo')}}" width="184px" height="69px"> {{$game->name}}</a></td>
+				<td style="background-color: {{$game->status->color}}">{{$game->status->name}}</td>
+				<td>{{$game->getFormattedPlaytime()}}</td>
+				<td>{{$game->notes}}</td>
 			</tr>
 		@endforeach
 	</tbody>
